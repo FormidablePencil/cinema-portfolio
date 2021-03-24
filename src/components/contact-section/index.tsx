8a3d867c3346225fc1d3c899c7836511bf2ca4eb
Dennis Bogdan Aleksandrov <extraordinarycoutesy@gmail.com>
2021-03-24 01:57:46 -0700

    fixed up contact page

--- src/components/contact-section/index.tsx
+++ src/components/contact-section/index.tsx
@@ -1,7 +1,6 @@
 import "./index.scoped.sass";
 import emailjs from "emailjs-com";
 import abstractImg from "../../assets/images/abstractImg.png";
-import { CSSTransition } from "react-transition-group";
 
 function ContactSection() {
   function sendEmail(e) {
@@ -21,20 +20,21 @@ function ContactSection() {
     <div className="container">
       <img src={abstractImg} alt="abstract" />
 
-      <form className="contact-form" onSubmit={sendEmail}>
-        {/* <input type="hidden" name="contact_number" /> */}
-        <label>Name</label>
-        <input type="text" name="from_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>
-          Number <span className="optional">optional</span>
-        </label>
-        <input type="text" name="user_number" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
-      </form>
+      <div className="form-container">
+        <form className="contact-form" onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="from_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>
+            Number <span className="optional">(optional)</span>
+          </label>
+          <input type="text" name="user_number" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input className="submit" type="submit" value="Send" />
+        </form>
+      </div>
     </div>
   );
 }
