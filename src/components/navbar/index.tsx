8cfd1b3f60d45924a0bb86f2a68bb4ea89d989b6
Dennis Bogdan Aleksandrov <extraordinarycoutesy@gmail.com>
2021-03-24 23:31:00 -0700

    now nav closes when tapped away

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -23,53 +23,62 @@ function Navbar() {
   };
 
   const toggleProjectsDropdown = () => setOpenProjects((prev) => !prev);
-  const onPressProjectItems = () => setNavbarOpen((prev) => !prev);
+  const closeNav = () => setNavbarOpen((prev) => !prev);
 
   return (
-    <div className="absolute-container">
-      <div className="container">
-        {isProject && <TabNavbar />}
-        <NavbarToggleBtn
-          navbarOpen={navbarOpen}
-          setNavbarOpen={setNavbarOpen}
-        />
-        <h1>Cinematographer</h1>
-        <ul className={navbarOpen ? "active" : ""}>
-          <li onClick={() => navigateTo("/")} className={`nav-item ${isHome}`}>
-            Home
-          </li>
-          <li
-            className={`nav-item ${isProject} hide-on-small`}
-            onClick={() => navigateTo("/projects/parallax")}
-          >
-            Projects
-          </li>
-          <li
-            onClick={toggleProjectsDropdown}
-            className={`nav-item ${
-              openProjects ? "open-projects" : "closed-projects"
-            } 
+    <>
+      <div className="absolute-container">
+        <div className="container">
+          {isProject && <TabNavbar />}
+          <NavbarToggleBtn
+            navbarOpen={navbarOpen}
+            setNavbarOpen={setNavbarOpen}
+          />
+          <h1>Cinematographer</h1>
+          <ul className={navbarOpen ? "active" : ""}>
+            <li
+              onClick={() => navigateTo("/")}
+              className={`nav-item ${isHome}`}
+            >
+              Home
+            </li>
+            <li
+              className={`nav-item ${isProject} hide-on-small`}
+              onClick={() => navigateTo("/projects/parallax")}
+            >
+              Projects
+            </li>
+            <li
+              onClick={toggleProjectsDropdown}
+              className={`nav-item ${
+                openProjects ? "open-projects" : "closed-projects"
+              } 
             ${isProject && "active"}
             hide-on-large`}
-          >
-            Projects
-            <IoIosArrowUp
-              size={24}
-              className={
-                openProjects ? "dropdown-icon-rotate" : "dropdown-icon"
-              }
-            />
-            <TabNavbar onPressItem={onPressProjectItems} inMainNavbar={true} />
-          </li>
-          <li
-            onClick={() => navigateTo("/contact")}
-            className={`nav-item ${isContact}`}
-          >
-            Contact
-          </li>
-        </ul>
+            >
+              Projects
+              <IoIosArrowUp
+                size={24}
+                className={
+                  openProjects ? "dropdown-icon-rotate" : "dropdown-icon"
+                }
+              />
+              <TabNavbar onPressItem={closeNav} inMainNavbar={true} />
+            </li>
+            <li
+              onClick={() => navigateTo("/contact")}
+              className={`nav-item ${isContact}`}
+            >
+              Contact
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+      <div
+        onClick={closeNav}
+        className={navbarOpen ? "on-tap-close-container" : ""}
+      />
+    </>
   );
 }
 
